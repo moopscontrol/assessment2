d525d0b32588c4d6ae0693251d10767da1562a38
MoopsControl <moopscontrol@gmail.com>
2022-11-06 13:04:49 +0530

    Input Method

--- src/au/torrens/assessment2/Main.java
+++ src/au/torrens/assessment2/Main.java
@@ -1,8 +1,25 @@
 package au.torrens.assessment2;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+
+    static double input(String msg) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("msg");
+        Double val = null;
+        while (val == null) {
+            try {
+                val = sc.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.print("Input was invalid. Try again: ");
+                sc.next(); // skip the invalid input
+            }
+        }
+
+        return val;
+    }
 
     static void sum(double a, double b) {
         System.out.println("The sum is >>> " + (a+b));
@@ -21,14 +38,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
 	    System.out.println("Simple Calculator");
-        System.out.println("Enter a number >>>");
-        double a = sc.nextDouble();
-
-        System.out.println("Enter a number >>>");
-        double b = sc.nextDouble();
+        double a = input("Enter a number >>>");
+        double b = input("Enter another number >>>");
 
         sum(a, b);
         diff(a, b);
